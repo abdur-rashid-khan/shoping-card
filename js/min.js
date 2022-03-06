8cedb32e3d2f7a017869490f41f0c7eab7a0380f
Rashid khan <rashidkhanbd57@gmail.com>
2022-03-06 11:59:09 +0600

    save localstoge data

--- js/min.js
+++ js/min.js
@@ -1,17 +1,42 @@
-const input=document.getElementById('input');
+
 const addBtn=document.getElementById('add-btn');
 const clear=document.getElementById('clear');
 
+function add(){
+   const input=document.getElementById('input');
+   const name=input.value;
+   // display in the ui
+   displayData(name);
+   // add to localstoreg
+   addProductsCard(name);
+
+   input.value='';
+}
+
+
 // display data 
-const  displayData=()=>{
-   let inputValue=input.value;
+const  displayData=name=>{
    // data show
    const products =document.getElementById('products');
    let li=document.createElement('li');
-   li.innerText=`${inputValue}`;
+   li.innerText=name;
    products.appendChild(li);
-   input.value='';
 }
-addBtn.addEventListener('click',()=>{
-   displayData();
-})
+
+const getCard=()=>{
+   let card=localStorage.getItem('card');
+   let cardObj;
+   if(card){
+      cardObj=JSON.parse(card);
+   }else{
+      cardObj={};
+   }
+   return cardObj;
+}
+const  addProductsCard=name=>{
+   const card =getCard();
+   card[name]=1;
+   // console.log(card);
+   let cardConvetStr=JSON.stringify(card);
+   localStorage.setItem('card',cardConvetStr);
+}
